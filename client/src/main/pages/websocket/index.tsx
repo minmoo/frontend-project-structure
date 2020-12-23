e67ad77d1155117e82be2241607cc1d2856ca9ed
minmoo <kimminsu951@hanmail.net>
2020-12-23 12:45:57 +0000

    snippets

--- client/src/main/pages/websocket/index.tsx
+++ client/src/main/pages/websocket/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Button, Card, CardContent, CardActions, Typography } from '@material-ui/core';
 import { useWsConnect } from '../../../modules/websocket';
